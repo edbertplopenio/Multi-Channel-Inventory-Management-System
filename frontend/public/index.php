fb884cc3401ce8a6c3575614e71187186d754b19
edbertplopenio <edbertplopenio@gmail.com>
2024-12-01 13:39:22 +0800

    added icon

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -68,6 +68,7 @@ mysqli_close($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main System</title>
     <link rel="stylesheet" href="styles/main.css">
+    <link rel="icon" href="../../frontend/public/images/bmsuiticon.png" type="image/png">
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.js"></script> <!-- Include jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
